003863655d3487d7dfa5c5f0bf257b687d207a2d
David Velasquez <tidusdavid@gmail.com>
2020-07-19 20:05:44 +0200

    implemented fetch data

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
 import "../styles/Map.css";
 
-var places = [
+var data = [
   {
     name: "Madrileña",
     position: {
@@ -25,7 +25,20 @@ var places = [
   },
 ];
 
-const getInfoWindowString = (place) => `
+export default class Map extends Component {
+  state = {
+    places: []
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  fetchData = () => {
+    this.setState({places: data})
+  }
+
+  getInfoWindowString = (place) => `
     <div>
       <div style="font-size: 16px;">
         ${place.name}
@@ -37,69 +50,70 @@ const getInfoWindowString = (place) => `
       </div>
     </div>`;
 
-const handleApiLoaded = (map, maps, places) => {
-  const markers = [];
-  const infowindows = [];
-  let activeInfoWindow = null;
-  places.forEach((place) => {
-    markers.push(
-      new maps.Marker({
-        position: {
-          lat: place.position.lat,
-          lng: place.position.lng,
-        },
-        icon: {
-          url: require(`../assets/air-blower-${place.status}.svg`),
-          scaledSize: new maps.Size(35, 35),
-          origin: new maps.Point(0, 0),
-        },
-        label: {
-          color: "#191970",
-          fontSize: "20px",
-          fontWeight: "bold",
-          text: place.numMachines.toString()
-        },
-        map,
-      })
+    handleApiLoaded = (map, maps, places) => {
+      const markers = [];
+      const infowindows = [];
+      let activeInfoWindow = null;
+      places.forEach((place) => {
+        markers.push(
+          new maps.Marker({
+            position: {
+              lat: place.position.lat,
+              lng: place.position.lng,
+            },
+            icon: {
+              url: require(`../assets/air-blower-${place.status}.svg`),
+              scaledSize: new maps.Size(35, 35),
+              origin: new maps.Point(0, 0),
+            },
+            label: {
+              color: "#191970",
+              fontSize: "20px",
+              fontWeight: "bold",
+              text: place.numMachines.toString(),
+            },
+            map,
+          })
+        );
+    
+        infowindows.push(
+          new maps.InfoWindow({
+            content: this.getInfoWindowString(place),
+          })
+        );
+      });
+    
+      map.addListener("click", () => {
+        activeInfoWindow && activeInfoWindow.close();
+        activeInfoWindow = null;
+      });
+    
+      markers.forEach((marker, i) => {
+        marker.addListener("click", () => {
+          activeInfoWindow && activeInfoWindow.close();
+          infowindows[i].open(map, marker);
+          activeInfoWindow = infowindows[i];
+        });
+      });
+    };
+
+  render() {
+    const {location, zoomLevel} = this.props;
+    return (
+      <div className="map">
+        <div className="google-map">
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: "" }}
+            defaultCenter={location}
+            defaultZoom={zoomLevel}
+            distanceToMouse={() => {}}
+            yesIWantToUseGoogleMapApiInternals
+            onGoogleApiLoaded={({ map, maps }) =>
+              this.handleApiLoaded(map, maps, this.state.places)
+            }
+          ></GoogleMapReact>
+        </div>
+      </div>
     );
-
-    infowindows.push(
-      new maps.InfoWindow({
-        content: getInfoWindowString(place),
-      })
-    );
-  });
-
-  map.addListener("click", () => {
-    activeInfoWindow && activeInfoWindow.close();
-    activeInfoWindow = null;
-  });
-
-  markers.forEach((marker, i) => {
-    marker.addListener("click", () => {
-      activeInfoWindow && activeInfoWindow.close();
-      infowindows[i].open(map, marker);
-      activeInfoWindow = infowindows[i];
-    });
-  });
-};
-
-export const Map = ({ location, zoomLevel }) => (
-  <div className="map">
-    <div className="google-map">
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
-        defaultCenter={location}
-        defaultZoom={zoomLevel}
-        distanceToMouse={() => {}}
-        yesIWantToUseGoogleMapApiInternals
-        onGoogleApiLoaded={({ map, maps }) =>
-          handleApiLoaded(map, maps, places)
-        }
-      >
-      </GoogleMapReact>
-    </div>
-  </div>
-);
-
-export default Map;
+  }
+}
